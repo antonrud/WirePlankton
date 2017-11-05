b15f3db395ab4e29c85469b5004b11b4549dc17b
Anton Rudacov <anton.rudacov@campus.tu-berlin.de>
2017-11-05 16:42:48 +0100

    Application and package names changed

---
+++ src/main/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/control/WirePlankton.java
@@ -0,0 +1,16 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.control;
+
+/**
+ * 
+ * @author Anton, Matthias, Stefan, Lana
+ *
+ */
+
+public class WirePlankton 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+    }
+}
