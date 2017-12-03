f361fe3132000883c29054252adafa2492fdfeeb
Matthias Lehmann <lehmann.2@campus.tu-berlin.de>
2017-12-03 22:26:07 +0100

    add more testcases

---
+++ src/test/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/control/ImportExportControllerUnitTest.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ImportExportControllerUnitTest {
+
+	@Test
+	public void doImportTest() {
+		
+		assertTrue(true); //not implemented yet
+	}
+
+}
---
+++ src/test/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/control/ViewControllerUnitTest.java
@@ -0,0 +1,24 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ViewControllerUnitTest {
+
+	@Test
+	public void StartTest() {
+		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void goTest() {
+		
+	}
+	
+	@Test
+	public void changeViewTest() {
+		
+	}
+}
