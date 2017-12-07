4e25b2ecebed93a779a1ede9b095402e1258324f
Matthias Lehmann <lehmann.2@campus.tu-berlin.de>
2017-12-07 10:57:58 +0100

    add two test cases and fitting packages

---
+++ src/test/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/model/PacketViewItemUnitTest.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PacketViewItemUnitTest {
+
+	@Test
+	public void test() {
+
+		assertTrue(true);
+	}
+
+}
---
+++ src/test/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/view/ExportViewUnitTest.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.view;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExportViewUnitTest {
+
+	@Test
+	public void test() {
+
+		assertTrue(true);
+	}
+
+}
