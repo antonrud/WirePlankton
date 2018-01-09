6f3171d73bd023b39322bed486801c0b370177d7
Stefan Pawlowski <Stefan.Pawlowski@infinithings.de>
2018-01-09 03:05:44 +0100

    Start filling settings classes

--- src/main/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/view/SingleChoice.java
+++ src/main/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/view/SingleChoice.java
@@ -1,5 +1,30 @@
 package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.view;
 
-public class SingleChoice {
+import java.util.ArrayList;
+
+import de.tuberlin.tubit.gitlab.lemannma.WirePlankton.model.Setting;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+public class SingleChoice extends VBox{
+
+	ObservableList<String> inputs;
+
+	Setting s;
+	ChoiceBox field;
+
+	public SingleChoice(String ddefault, ArrayList<String> choices){
+
+		inputs.addAll(choices);
+		field = new ChoiceBox<String>(inputs);
+
+
+		this.getChildren().add(field);
+
+
+
+	}
 
 }
