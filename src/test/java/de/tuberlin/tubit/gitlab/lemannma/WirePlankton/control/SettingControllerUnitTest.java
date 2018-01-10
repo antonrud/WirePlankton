f893597cfcd71a1a5fa32c832fd72cdcae266e21
Matthias Lehmann <lehmann.2@campus.tu-berlin.de>
2018-01-10 11:23:30 +0100

    add more basic unit tests

---
+++ src/test/java/de/tuberlin/tubit/gitlab/lemannma/WirePlankton/control/SettingControllerUnitTest.java
@@ -0,0 +1,15 @@
+package de.tuberlin.tubit.gitlab.lemannma.WirePlankton.control;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SettingControllerUnitTest {
+
+	@Test
+	public void test() {
+	
+		assertTrue(true); //not implemented et
+	}
+
+}
